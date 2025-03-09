865eeaf01a94df3e72a49583f790890a12746c2a
jonathanbasuki <basukijonathan4@gmail.com>
2025-03-09 23:41:50 +0700

    add crud for saved campsite

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const userRoute = require('./routes/user_route');
-const bookmarkRoute = require('./routes/bookmark_route');
+const savedRoute = require('./routes/saved_route');
 const bookingRoute = require('./routes/booking_route');
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 // Routing
 app.use(userRoute);
-// app.use(bookmarkRoute);
+app.use(savedRoute);
 // app.use(bookingRoute);
 
 // Menjalankan server
