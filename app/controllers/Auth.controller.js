59ceb37be3d18b1914bc3fa0834bf24df2cc3f7d
PIYANOOO <sufyanalafi@gmail.com>
2025-04-20 00:12:39 +0700

    feat: implement using authentication using jwt and session

--- app/controllers/Auth.controller.js
+++ app/controllers/Auth.controller.js
@@ -1,4 +1,78 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User.model');
+
 // Login page route
 exports.renderLoginPage = (req, res) => {
-    res.render('pages/auth/login', { title: "Login" });
+    res.render('pages/auth/login', {
+        title: "Login",
+        error: null,
+        scripts: []
+    });
 }
+
+// Validate user login 
+exports.validateLogin = async (req, res) => {
+    const { identifier, password } = req.body;
+
+    try {
+        const user = await User.getUserByEmailOrUsername(identifier);
+
+        if (!user) {
+            return res.render('pages/auth/login', {
+                title: 'Login',
+                error: 'Credentials not found.',
+                scripts: []
+            });
+        }
+
+        const match = await bcrypt.compare(password, user.password_hash);
+
+        if (!match) {
+            return res.render('pages/auth/login', {
+                title: 'Login',
+                error: 'Invalid credentials. Please try again.',
+                scripts: []
+            });
+        }
+
+        const token = jwt.sign({
+            id: user.user_id,
+            username: user.username,
+            email: user.email
+        }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        // Simpan token ke cookie (opsional)
+        res.cookie('token', token, { httpOnly: true });
+        res.redirect('/dashboard');
+    } catch (err) {
+        res.render('pages/auth/login', {
+            title: 'Login',
+            error: 'Something went wrong. Please check your credentials.',
+            scripts: []
+        });
+    }
+};
+
+// Register page route
+exports.renderRegisterPage = (req, res) => {
+    res.render('pages/auth/register', {
+        title: 'Register',
+        scripts: ['/js/auth/register.js']
+    });
+}
+
+// Forgot password page route
+exports.renderForgotPage = (req, res) => {
+    res.render('pages/auth/forgot_password', {
+        title: "Forgot Password",
+        scripts: []
+    });
+}
+
+// Handle user logout
+exports.logout = (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/login');
+};
--- app/routes/Auth.route.js
+++ app/routes/Auth.route.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require('../controllers/Auth.controller');
+const { validationResult } = require('express-validator');
 
-router.get('/login', authController.renderLoginPage);
+const authController = require('../controllers/Auth.controller');
+const userController = require('../controllers/User.controller');
+
+const authValidation = require('../middleware/authValidator');
+const authAuthenticator = require('../middleware/authAuthenticator');
+
+router.get('/login', authAuthenticator.redirectIfAuthenticated, authController.renderLoginPage);
+router.post('/login', authController.validateLogin);
+
+router.get('/register', authAuthenticator.redirectIfAuthenticated, authController.renderRegisterPage);
+router.post('/register', authValidation.registerValidation,
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // Return validation errors as JSON
+            return res.status(422).json({
+                status: 422,
+                message: 'Validation failed',
+                errors: errors.array()
+            });
+        }
+
+        next();
+    },
+    userController.createUser
+);
+
+router.get('/forgot-password', authAuthenticator.redirectIfAuthenticated, authController.renderForgotPage);
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
