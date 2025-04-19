7ca791023c28c0c4f9a1917261f54c74cf573f0d
DindaElrossa <dindaelrossa3875@gmail.com>
2025-04-19 23:17:20 +0700

    remove: duplicated conflict file

--- app/routes/Home.route.js.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const homeController = require('../controllers/Home.controller');
-
-router.get('/', homeController.renderHomePage);
-router.get('/about', homeController.renderAboutPage);
-
-module.exports = router;
